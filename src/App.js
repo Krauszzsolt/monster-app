269c2b49b6e542f87ab56a104ead881d6c6ca199
Krausz Zsolt <36264501+Krauszzsolt@users.noreply.github.com>
2021-11-22 14:19:19 +0100

    Initial

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import './App.css';
+import { Component } from 'react';
+import { CardList } from './components/card-list/card-list.component';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+      searchField: '',
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users }));
+  }
+
+  render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter((monster) =>
+      monster.name.toLowerCase().includes(searchField.toLocaleLowerCase())
+    );
+    return (
+      <div className='App'>
+        <input type='search' placeholder='search' onChange={(e) => this.setState({ searchField: e.target.value })} />
+        <CardList>{filteredMonsters}</CardList>;
+      </div>
+    );
+  }
+}
+
+export default App;
